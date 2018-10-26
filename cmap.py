7c12466aa95198967625c294cc53327b158f75d4
Unknown <camcaswell@gmail.com>
2018-10-26 02:17:12 -0400

    added preset colors

--- cmap.py
+++ cmap.py
@@ -38,3 +38,15 @@ def random_lc_cmap(count=2, seed=None):
 ## RGB values should be 0-255
 
 BW = cmformat([255, 255, 255], [0,0,0])
+WB = cmformat([0,0,0], [255, 255, 255])
+LEAFLIFE = cmformat([182, 249, 214], [131, 209, 94])
+
+HARVEST = cmformat([197, 77, 0], [155, 207, 81], [252, 196, 0])
+FARHARVEST = cmformat([58, 124, 97], [241, 227, 24], [220, 38, 8])
+OLDSAND = cmformat([248, 216, 110], [161, 59, 17], [223, 117, 0])
+ALGAE = cmformat([129, 199, 144], [19, 148, 235], [161, 232, 255])
+CARROTFIELD = cmformat([221, 103, 16], [68, 123, 45], [109, 106, 44])
+FRIENDLYVOID = cmformat([82, 116, 254], [185, 229, 239], [218, 104, 0])
+COLDFUSION = cmformat([46, 7, 225], [0, 255, 111], [109, 225, 241])
+LIGHTABYSS = cmformat([51, 57, 242], [121, 116, 242], [23, 55, 154])
+LABLIFE = cmformat([208, 62, 153], [46, 162, 39], [154, 255, 189])
